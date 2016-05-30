38eee07cd7dc3eff5e82c10f166c7dc68714ddd1
patricioarango <patricioarango@gmail.com>
2016-05-30 08:12:41 -0300

    two

--- www/js/controles.js
+++ www/js/controles.js
@@ -10,9 +10,96 @@ aplicacion.filter('getById', function() {
   }
 });
 
-aplicacion.controller('carritoCtrl',['$scope','$location','$http', function($scope, $location,$http){
-      console.log("aca");
+aplicacion.factory('registroCantidadVentas',function(){
+  var items = [];
+  var itemsService = {};
+
+  if (localStorage.getItem("laslilas_nro_ventas") === null) {
+    localStorage.setItem("laslilas_nro_ventas",0);
+  }
+
+  itemsService.add = function() {
+    var nro_vta = localStorage.getItem("laslilas_nro_ventas");
+    var nueva_vta = (Number(nro_vta) + Number(1));
+    localStorage.setItem("laslilas_nro_ventas", nueva_vta);
+    items[0] = nueva_vta;
+  };
+
+  itemsService.list = function() {
+    items[0] = localStorage.getItem("laslilas_nro_ventas");
+      return items;
+  };
+
+  itemsService.delete = function() {
+    /*var nro_vta = localStorage.getItem("laslilas_nro_ventas");
+    var nueva_vta = (Number(nro_vta) - Number(1));
+    localStorage.setItem("laslilas_nro_ventas", nueva_vta);
+    items[0] = nueva_vta;*/
+  };
+
+  return itemsService;
+});
+
+aplicacion.factory('registroVentas',function(){
+
+  if (localStorage.getItem("laslilas_nro_ventas") >= 1){
+    var items = JSON.parse(localStorage.getItem("laslilas_listado_ventas"));
+  } else {
+    var items = [];
+  }
+    
+
+  var itemsService = {};
+  var venta = {};
+    
+  itemsService.add = function(detalle) {
+    ventaid = "ventaid_" + localStorage.getItem("laslilas_nro_ventas");
+    detalle.ventaidparaborrar = ventaid;
+    venta[ventaid] = detalle;
+    items.push(venta);
+    localStorage.setItem("laslilas_listado_ventas",JSON.stringify(items));
+  };
+
+  itemsService.delete = function(key) {
+    console.log(localStorage.getItem("laslilas_listado_ventas",JSON.parse(items)));
+  };
+
+  itemsService.list = function() {
+    return items;
+  };
+    
+  return itemsService;
+});
+
+aplicacion.controller('carritoCtrl',['$scope','$location','$http','registroVentas','registroCantidadVentas', function($scope,$location,$http,registroVentas,registroCantidadVentas){
+
+//localStorage.removeItem("laslilas_nro_ventas");
+//localStorage.removeItem("laslilas_listado_ventas");
+
+      $scope.listado_ventas = registroVentas.list();
+
+      if ($scope.listado_ventas == ""){
+        $scope.no_ventas = true;
+      }
+
+      var returnArr = [];
+      angular.forEach($scope.listado_ventas, function(value,key) {
+        angular.forEach(value, function(value2,key2) {
+          returnArr.push(value2);
+        });
+      });
+      $scope.listado_ventas = returnArr;
+
+      //para eliminar ventas
+      $scope.borrarVenta = function(key){
+        registroCantidadVentas.delete();
+        registroVentas.delete(key);
+      }
   }]);
+
+aplicacion.controller('navegacionCtrl',['$scope','registroCantidadVentas', function($scope,registroCantidadVentas){
+       $scope.ventasActuales = registroCantidadVentas.list();
+}]);
 
 aplicacion.controller('catalogoCtrl',['$scope','$location','$http', function($scope, $location,$http){
     //localStorage.setItem("rc2016_firstime","0");
@@ -48,18 +135,20 @@ aplicacion.controller('subcatalogoCtrl',['$scope', '$routeParams', '$http','$sce
       });
   }]);
 
-aplicacion.controller('detalleCtrl',['$scope', '$routeParams', '$http','$sce','$rootScope','$filter',
-    function($scope, $routeParams, $http,$sce,$rootScope,$filter) {
+aplicacion.controller('detalleCtrl',['$scope', '$routeParams', '$http','$sce','$rootScope','$filter','registroVentas','registroCantidadVentas',function($scope, $routeParams, $http,$sce,$rootScope,$filter,registroVentas,registroCantidadVentas) {
       $scope.id_toro = $routeParams.id_detalle;
+
       $http.get('laslilas.json').success(function(data){
         var found = $filter('getById')(data, $scope.id_toro);
-         console.log(found);
-         //$scope.selected = JSON.stringify(found);
-
-
         $scope.detalle = found;
       });
-  }]);
+
+      $scope.agregarVenta = function (){
+        registroCantidadVentas.add();
+        registroVentas.add($scope.detalle);
+      };
+
+}]);
 
 aplicacion.controller('filtrosCtrl',['$scope', '$routeParams', '$http','$sce','$rootScope','$filter',
     function($scope, $routeParams, $http,$sce,$rootScope,$filter) {
